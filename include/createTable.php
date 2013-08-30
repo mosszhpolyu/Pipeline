fcc0a2896f20272adda5c7cddbc732589320b177
mosszhpolyu <mosszhpolyu@gmail.com>
2013-08-30 20:49:42 +0800

    data type changed
    varchar => VARCHAR
    float => FLOAT

--- include/createTable.php
+++ include/createTable.php
@@ -26,31 +26,32 @@
 	// gene_jobID table
 	// probe_id varchar(255), gene_name varchar(255)
 	//////////////////////////////////
-	$geneTableSchema = '{' . $probe_id . 'varchar(255)' . ',' . $gene_name . 'varchar255' . '}';
+	$geneTableSchema = '{' . $probe_id . 'VARCHAR(255)' . ',' . $gene_name . 'VARCHAR(255)' . '}';
 
 	//////////////////////////////////
 	// expressionNormal table
 	// probe_id varchar(255), sample_id varchar(255), expression_level float
 	//////////////////////////////////
-	$expressionNormalTableSchema = '{' . $probe_id . 'varchar(255)' . ',' . $sample_id . 'varchar(255)' . $expression_level . 'float' . '}';
+	$expressionNormalTableSchema = '{' . $probe_id . 'VARCHAR(255)' . ',' . $sample_id . 'VARCHAR(255)' . $expression_level . 'FLOAT' . '}';
 
 	//////////////////////////////////
 	// expressionDisease table
 	// probe_id varchar(255), sample_id varchar(255), expression_level float
 	//////////////////////////////////
-	$expressionDiseaseTableSchema = '{' . $probe_id . 'varchar(255)' . ',' . $sample_id . 'varchar(255)' . $expression_level . 'float' . '}';
+	$expressionDiseaseTableSchema = '{' . $probe_id . 'VARCHAR(255)' . ',' . $sample_id . 'VARCHAR(255)' . $expression_level . 'FLOAT' . '}';
 
 	//////////////////////////////////
 	// correlationNormal table
 	// probe1_id varchar(255), probe2_id varchar(255), correlation_R float, correlation_P float
 	//////////////////////////////////
-	$correlationNormalTableSchema = '{' . $probe1_id . 'varchar(255)' . ',' . $probe2_id . 'varchar(255)' . ',' . $correlation_R . 'float' . ',' . $correlation_P . 'float' . '}';
+	$correlationNormalTableSchema = '{' . $probe1_id . 'VARCHAR(255)' . ',' . $probe2_id . 'VARCHAR(255)' . ',' . $correlation_R . 'FLOAT' . ',' . $correlation_P . 'FLOAT' . '}';
 
 	//////////////////////////////////
 	// correlationDisease table
 	// probe1_id varchar(255), probe2_id varchar(255), correlation_R float, correlation_P float
 	//////////////////////////////////
-	$correlationDiseaseTableSchema = '{' . $probe1_id . 'varchar(255)' . ',' . $probe2_id . 'varchar(255)' . ',' . $correlation_R . 'float' . ',' . $correlation_P . 'float' . '}';
+	$correlationDiseaseTableSchema = '{' . $probe1_id . 'VARCHAR(255)' . ',' . $probe2_id . 'VARCHAR(255)' . ',' . $correlation_R . 'FLOAT' . ',' . $correlation_P . 'FLOAT' . '}';
+
 
 	//////////////////////////////////
 	// create tables SQL state
@@ -60,6 +61,12 @@
 	$expressionDiseaseTableSQL = 'CREATE TABLE' . $expressionDiseaseTableName . $expressionDiseaseTableSchema;
 	$correlationNormalTableSQL = 'CREATE TABLE' . $correlationNormalTableName . $expressionNormalTableSchema;
 	$correlationDiseaseTableSQL = 'CREATE TABLE' . $correlationDiseaseTableName . $expressionDiseaseTableSchema;
+
+	/////////////////////////////////
+	// alter table
+	// add primary key, foreign key
+	/////////////////////////////////
+
 
 	/////////////////////////////////
 	// execute the SQL statements
