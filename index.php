08285f6b75821b892d1483419362f5c926793688
mosszhpolyu <mosszhpolyu@gmail.com>
2013-09-16 20:53:17 +0800

    Added index.php
    dummy website to handle incoming excel data

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<form action="prepareData.php" method="post" target="_blank" enctype="multipart/form-data">
+			<label for="file">Filename:</label>
+			<input type="file" name="file" id="file"><br>
+			<input type="submit" name="submit" value="Submit">
+		</form>
+	</body>
+</html>
+
+<html>
+<body>
