f689e1a7aaf16b429d5773e60e80bab3b2a5a71d
mosszhpolyu <mosszhpolyu@gmail.com>
2013-09-19 16:07:09 +0800

    added include\createTable.php
    1. create table names as JobID_common_table_name
    2. change mysql_query to mysqli_query

--- include/createTable.php
+++ include/createTable.php
@@ -1,10 +1,16 @@
 <?php
+
+	require('./config.php');
+
+	// constant SQL statement create table
+	$SQL_CREATE_TABLE = 'CREATE TABLE ';
+	
 	// Assign table names, which are identified by jobID
-	$geneTableName = 'gene_' . $jobID;
-	$expressionNormalTableName = 'expression_normal_' . $jobID;
-	$expressionDiseaseTableName = 'expression_disease_' . $jobID;
-	$correlationNormalTableName = 'correlation_normal_' . $jobID;
-	$correlationDiseaseTableName = 'correlation_disease_' . $jobID;
+	$geneTableName = $jobID . '_gene';
+	$expressionNormalTableName = $jobID . '_expression_normal';
+	$expressionDiseaseTableName = $jobID . '_expression_disease';
+	$correlationNormalTableName = $jobID . '_correlation_normal';
+	$correlationDiseaseTableName = $jobID . '_correlation_disease';
 
 	//////////////////////////////////
 	// constant values for table schema
@@ -13,8 +19,8 @@
 	$gene_name = 'gene_name';
 	$sample_id = 'sample_id';
 	$expression_level = 'expression_level';
-	$probe1_id = 'probe1_id';
-	$probe2_id = 'probe2_id';
+	$gene_name_1 = 'gene_name_1';
+	$gene_name_2 = 'gene_name_1';
 	$correlation_R = 'correlation_R';
 	$correlation_P = 'correlation_P';
 
@@ -26,41 +32,40 @@
 	// gene_jobID table
 	// probe_id varchar(255), gene_name varchar(255)
 	//////////////////////////////////
-	$geneTableSchema = '{' . $gene_name . 'VARCHAR(255)' . $probe_id . 'VARCHAR(255)' . ',' . '}';
+	$geneTableSchema = ' (' . $gene_name . ' VARCHAR(255)' . ', ' . $probe_id . ' VARCHAR(255)' . ')';
 
 	//////////////////////////////////
 	// expressionNormal table
 	// gene_name varchar(255), sample_id varchar(255), expression_level float
 	//////////////////////////////////
-	$expressionNormalTableSchema = '{' . $gene_name . 'VARCHAR(255)' . ',' . $sample_id . 'VARCHAR(255)' . $expression_level . 'FLOAT' . '}';
+	$expressionNormalTableSchema = ' (' . $gene_name . ' VARCHAR(255)' . ', ' . $sample_id . ' VARCHAR(255)' . ', ' . $expression_level . ' FLOAT' . ')';
 
 	//////////////////////////////////
 	// expressionDisease table
 	// gene_name varchar(255), sample_id varchar(255), expression_level float
 	//////////////////////////////////
-	$expressionDiseaseTableSchema = '{' . $gene_name . 'VARCHAR(255)' . ',' . $sample_id . 'VARCHAR(255)' . $expression_level . 'FLOAT' . '}';
+	$expressionDiseaseTableSchema = ' (' . $gene_name . ' VARCHAR(255)' . ', ' . $sample_id . ' VARCHAR(255)' . ', ' . $expression_level . ' FLOAT' . ')';
 
 	//////////////////////////////////
 	// correlationNormal table
 	// gene_name_1 varchar(255), gene_name_2 varchar(255), correlation_R float, correlation_P float
 	//////////////////////////////////
-	$correlationNormalTableSchema = '{' . $gene_name_1 . 'VARCHAR(255)' . ',' . $gene_name_2 . 'VARCHAR(255)' . ',' . $correlation_R . 'FLOAT' . ',' . $correlation_P . 'FLOAT' . '}';
+	$correlationNormalTableSchema = ' (' . $gene_name_1 . ' VARCHAR(255)' . ', ' . $gene_name_2 . ' VARCHAR(255)' . ', ' . $correlation_R . ' FLOAT' . ', ' . $correlation_P . ' FLOAT' . ')';
 
 	//////////////////////////////////
 	// correlationDisease table
 	// gene_name_1 varchar(255), gene_name_2 varchar(255), correlation_R float, correlation_P float
 	//////////////////////////////////
-	$correlationDiseaseTableSchema = '{' . $gene_name_1 . 'VARCHAR(255)' . ',' . $gene_name_2 . 'VARCHAR(255)' . ',' . $correlation_R . 'FLOAT' . ',' . $correlation_P . 'FLOAT' . '}';
-
+	$correlationDiseaseTableSchema = ' (' . $gene_name_1 . ' VARCHAR(255)' . ', ' . $gene_name_2 . ' VARCHAR(255)' . ', ' . $correlation_R . ' FLOAT' . ', ' . $correlation_P . ' FLOAT' . ')';
 
 	//////////////////////////////////
 	// create tables SQL state
 	//////////////////////////////////
-	$geneTableSQL = 'CREATE TABLE' . $geneTableName . $geneTableSchema;
-	$expressionNormalTableSQL = 'CREATE TABLE' . $expressionNormalTableName . $expressionNormalTableSchema;
-	$expressionDiseaseTableSQL = 'CREATE TABLE' . $expressionDiseaseTableName . $expressionDiseaseTableSchema;
-	$correlationNormalTableSQL = 'CREATE TABLE' . $correlationNormalTableName . $expressionNormalTableSchema;
-	$correlationDiseaseTableSQL = 'CREATE TABLE' . $correlationDiseaseTableName . $expressionDiseaseTableSchema;
+	$geneTableSQL = $SQL_CREATE_TABLE . $geneTableName . $geneTableSchema;
+	$expressionNormalTableSQL = $SQL_CREATE_TABLE . $expressionNormalTableName . $expressionNormalTableSchema;
+	$expressionDiseaseTableSQL = $SQL_CREATE_TABLE . $expressionDiseaseTableName . $expressionDiseaseTableSchema;
+	$correlationNormalTableSQL = $SQL_CREATE_TABLE . $correlationNormalTableName . $expressionNormalTableSchema;
+	$correlationDiseaseTableSQL = $SQL_CREATE_TABLE . $correlationDiseaseTableName . $expressionDiseaseTableSchema;
 
 	/////////////////////////////////
 	// alter table
@@ -71,9 +76,9 @@
 	/////////////////////////////////
 	// execute the SQL statements
 	/////////////////////////////////
-	mysql_query($geneTableSQL);
-	mysql_query($expressionNormalTableSQL);
-	mysql_query($expressionDiseaseTableSQL);
-	mysql_query($correlationNormalTableSQL);
-	mysql_query($correlationDiseaseTableSQL);
+	mysqli_query($DATABASE_LINK, $geneTableSQL);
+	mysqli_query($DATABASE_LINK, $expressionNormalTableSQL);
+	mysqli_query($DATABASE_LINK, $expressionDiseaseTableSQL);
+	mysqli_query($DATABASE_LINK, $correlationNormalTableSQL);
+	mysqli_query($DATABASE_LINK, $correlationDiseaseTableSQL);
 ?>
