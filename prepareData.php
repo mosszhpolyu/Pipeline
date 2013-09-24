1155c1f4d579e193e0ca283cbfc1d7c4e4039cd0
mosszhpolyu <mosszhpolyu@gmail.com>
2013-09-24 16:58:53 +0800

    modified prepareData.php

--- prepareData.php
+++ prepareData.php
@@ -4,7 +4,7 @@
 	require('./class/PHPExcel/Reader/Excel2007.php');
 
 	// database connection file path
-	//require('./config.php');
+	require('./config.php');
 
 	// create Job Id
 	require('./createJobID.php');
@@ -18,24 +18,10 @@
 
 	// Regular expression patterns
 	$PATTERN_NON_WHITE_SPACE = '/^[^\s]+$/';
-	$PATTERN_SAMPLE_ID;
+	$PATTERN_SAMPLE_ID = '/^\w+$/';
 	$PATTERN_GENE_NAME;
 	$PATTERN_PROBE_ID;
 	$PATTERN_EXPRESSION_VALUE;
-
-	//////////////////////////////
-	// create 5 tables
-	// gene_jobID
-	// expression_normal_jobID
-	// expression_disease_jobID
-	// correlation_normal_jobID
-	// correlation_disease_jobID
-	////////////////////////////////
-	$geneTableName;
-	$expressionNormalTableName;
-	$expressionDiseaseTableName;
-	$correlationNormalTableName;
-	$correlationDiseaseTableName;
 
 	///////////////////////////////
 	// variables to store the highest row and column of the dataset
@@ -49,17 +35,23 @@
 	$currentRow;
 	$currentCol;
 
-	/////////////////////////////
-	// array to store sample_ids and gene_jobID table data
-	/////////////////////////////
-	$sampleIdNormal;
-	$sampleIdDisease;
-	$geneData;
+	//////////////////////////////
+	// variable to store current read value from excel
+	//////////////////////////////
+	$currentData;
 
 	/////////////////////////////
-	// variable to store current data read from Excel's current cell
+	// array to store sample_ids, gene_jobID, and gene_expression table data
 	/////////////////////////////
-	$currentData;
+	$sampleId = array();
+	$geneData = array();
+	$expressionData = array();
+
+	/////////////////////////////
+	// variable to store current expression table name
+	// either $expressionNormalTableName or $expressionDiseaseTableName
+	/////////////////////////////
+	$expressionTableName;
 
 	// check if two worksheets have the same number of rows
 	if($xlsxFile->setActiveSheetIndex('0')->getHighestRow() != $xlsxFile->setActiveSheetIndex('1')->getHighestRow()) {
@@ -88,39 +80,40 @@
 				if($currentRow == 1) {
 					continue;
 				}
+
 				// second row is always sample schema
 				// store these sample_id to array
 				else if($currentRow == 2) {
 					switch ($sheetIndex) {
+						// Normal case
 						case '0':
-							for($currentColStr = 'A'; $currentColStr <= $currentSheet->getHighestColumn(); ++$currentColStr) {
-								// PHPExcel_Cell::columnIndexFromString($currentColStr) returns order of alphabet
-								// A is 1
-								// since column index starts from 0, we need to decrease one
-								$currentCol = PHPExcel_Cell::columnIndexFromString($currentColStr) - 1;
-								// get current cell's value
-								$sampleIdNormal[$currentCol] = $currentSheet->getCellByColumnAndRow($currentCol, $currentRow)->getValue();
-								echo $sampleIdNormal[$currentCol] . ',';
-							}
+							$expressionTableName = $expressionNormalTableName;
 							break;
-					
+							
+						// Disease case
 						case '1':
-							for($currentColStr = 'A'; $currentColStr <= $currentSheet->getHighestColumn(); $currentColStr++) {
-								// PHPExcel_Cell::columnIndexFromString($currentColStr) returns order of alphabet
-								// A is 1
-								// since column index starts from 0, we need to decrease one
-								$currentCol = PHPExcel_Cell::columnIndexFromString($currentColStr) - 1;
-								// get current cell's value
-								$sampleIdDisease[$currentCol] = $currentSheet->getCellByColumnAndRow($currentCol, $currentRow)->getValue();
-								echo $sampleIdDisease[$currentCol] . ',';
-							}
+							$expressionTableName = $expressionDiseaseTableName;
 							break;
 
 						default:
-							# code...
 							break;
 					} // end switch
-					//
+
+					// read each column of the current sample ID row
+					for($currentColStr = 'A'; $currentColStr <= $currentSheet->getHighestColumn(); $currentColStr++) {
+						// PHPExcel_Cell::columnIndexFromString($currentColStr) returns order of alphabet
+						// A is 1
+						// since column index starts from 0, we need to decrease one
+						$currentCol = PHPExcel_Cell::columnIndexFromString($currentColStr) - 1;
+						// get current cell's value
+						$currentData = $currentSheet->getCellByColumnAndRow($currentCol, $currentRow)->getValue();
+						// check if current value matches sample ID pattern
+						if(preg_match($PATTERN_SAMPLE_ID, $currentData)) {
+							//echo $currentData . ',';
+							// add this sample ID to the array
+							array_push($sampleId, $currentData);
+						}
+					} // end for
 				} // end if
 
 				// for the rest of the rows
@@ -132,49 +125,30 @@
 						// A is 1
 						// since column index starts from 0, we need to decrease one
 						$currentCol = PHPExcel_Cell::columnIndexFromString($currentColStr) - 1;
+						$currentData = trim($currentSheet->getCellByColumnAndRow($currentCol, $currentRow)->getCalculatedValue());
 						// the first two columns are gene name and probe id
-						// we store them in a temp array
+						// we store them in a geneData array
 						if($currentCol == 0 || $currentCol == 1) {
-							$geneData[$currentCol] = trim($currentSheet->getCellByColumnAndRow($currentCol, $currentRow)->getCalculatedValue());
-							if(preg_match($PATTERN_NON_WHITE_SPACE, $geneData[$currentCol])) {
-								echo $geneData[$currentCol] . ',';
-							}
+							array_push($geneData, $currentData);
 						}
 						// starting from 3rd column, it is the data
 						else {
-							$currentData = trim($currentSheet->getCellByColumnAndRow($currentCol, $currentRow)->getCalculatedValue());
-							if(preg_match($PATTERN_NON_WHITE_SPACE, $currentData)) {
-								echo $currentData . ',';
-							}
+							array_push($expressionData, $currentData);
 						}
-						// code to insert into gene table
-						// ?????????????????????????????
-						// complete code here
-						switch ($sheetIndex) {
-							case '0':
-								// insert into expressionNormalTable
-								// ?????????????????????????????
-								// complete code here
-								break;
-							case '1':
-								// insert into expressionDiseaseble
-								// ?????????????????????????????
-								// complete code here
-								break;
-							default:
-								# code...
-								break;
-						}
-					} // end for, we now have one row of data inserted
+					} // end for, we now have one row of data read
+					include('./include/insertData.php');
+					// reset geneData and expressionData arrays
+					$geneData = array();
+					$expressionData = array();
 				}
 				echo "\n";
 			} // end for, we now have all rows read in one worksheet
 			echo "<html><br></html>";
+			// reset the sampleId array
+			$sampleId = array();
 		} // end read sheet for, we now have both normal and disease data read and inserted to database
 	} // end else checking if the have the same number of rows
 
 	// need a way to do memory management
 	// ???????????????????????????????????
-
-	
 ?>
